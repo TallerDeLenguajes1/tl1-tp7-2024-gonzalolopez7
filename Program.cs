5c3ad1bd1093ac8dab823dd07fef669d52afbc85
Gonzalo <gonzalopez723@gmail.com>
2024-06-04 14:49:29 -0300

    2.c.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
 ﻿using EspacioEmpleado;
 
-var nuevoEmpleado = new Empleado("gonzalo", "lopez", new DateTime(2005,6,1), 's', new DateTime(2010, 6, 1), 1200, Cargos.Ingeniero);
-
-Console.WriteLine("antiguedad: " + nuevoEmpleado.Antiguedad());
-
-Console.WriteLine("edad: " + nuevoEmpleado.Edad());
-
-Console.WriteLine("años para jubilarse: " + nuevoEmpleado.AñosParaJubilarse());
-
-Console.WriteLine("salario: " + nuevoEmpleado.Salario());
+Empleado[] empleados = {
+    new Empleado("nombre1", "apellido1", new DateTime(2000, 1, 1), 's', new DateTime(2010, 1, 1), 200000, Cargos.Auxiliar),
+    new Empleado("nombre2", "apellido2", new DateTime(1995, 1, 1), 'c', new DateTime(2003, 1, 1), 250000, Cargos.Ingeniero),
+    new Empleado("nombre3", "apellido3", new DateTime(2005, 1, 1), 's', new DateTime(2020, 1, 1), 300000, Cargos.Especialista)
+    };
